ae73598badeea7aa811fdde866fdc777f6f389fc
MACKAY Jill <jmackay4@ed.ac.uk>
2020-09-09 17:19:06 +0100

    add ruk data

--- R/read_data.R
+++ R/read_data.R
@@ -2,7 +2,32 @@
 
 library(tidyverse)
 
-scot_names <- readxl::read_excel("~/data/babies-first-names-19-table5.xlsx",
+
+
+ruk_boys <-readxl::read_excel("data/2019boysnames.xlsx", 
+                               sheet = "Table 6", skip = 4) %>% 
+  select(rank = Rank,
+         names = Name,
+         number = Count3) %>% 
+  mutate(gender = "boys",
+         country = "rUK")
+
+ruk_girls <-readxl::read_excel("data/2019girlsnames.xlsx", 
+                               sheet = "Table 6", skip = 4) %>% 
+  select(rank = Rank,
+         names = Name,
+         number = Count3) %>% 
+  mutate(gender = "girls",
+         country = "rUK")
+
+ruk_names <- ruk_girls %>% 
+  full_join(ruk_boys) %>% 
+  mutate(rank = as.character(rank))
+
+rm(ruk_boys, ruk_girls)
+
+
+scot_names <- readxl::read_excel("~/data/ScottishNames/data/babies-first-names-19-table5.xlsx",
                             skip = 4) %>% 
   janitor::clean_names() %>% 
   rename(boy_rank = rank1_1,
@@ -13,21 +38,28 @@ scot_names <- readxl::read_excel("~/data/babies-first-names-19-table5.xlsx",
 
 girls <- scot_names %>% 
   select(girl_rank, girls_names, girl_number) %>% 
-  mutate(gender = "girls") %>% 
+  mutate(gender = "girls",
+         country = "Scotland") %>% 
   filter(!is.na(girls_names)) %>% 
   rename(names = girls_names,
          rank = girl_rank,
          number = girl_number)
 boys <- scot_names %>% 
   select(boy_rank, boys_names, boy_number) %>% 
-  mutate(gender = "boys") %>% 
+  mutate(gender = "boys",
+         country = "Scotland") %>% 
   filter(!is.na(boys_names)) %>% 
   rename(names = boys_names,
          rank = boy_rank,
          number = boy_number)
-rm(girls, boys)
+
 
 scot_names<- girls %>% 
   full_join(boys)
 
-write.csv("~/data/scot_names.csv", x = scot_names)
+rm(girls, boys)
+
+all_names <- scot_names %>% 
+  full_join(ruk_names)
+
+write.csv("~/data/ScottishNames/data/all_names.csv", x = all_names)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ScottishNames
 
 Data from National Records of Scotland [Table 5](https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/names/babies-first-names/babies-first-names-2019/list-of-data-tables)
+
+
+And Office National Statistics https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/bulletins/babynamesenglandandwales/2019#baby-names-data
