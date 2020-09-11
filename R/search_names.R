e887a95dd195e5870f1bac631ae4fcc93a255da1
MACKAY Jill <jmackay4@ed.ac.uk>
2020-09-11 16:48:11 +0100

    finalised function

--- R/search_names.R
+++ R/search_names.R
@@ -2,66 +2,30 @@
 
 library(tidyverse)
 
-scot_names <- read.csv("data/scot_names.csv") %>% 
-  select(-X) %>% 
-  mutate(names = tolower(names))
-
 all_names <- read.csv("data/all_names.csv") %>% 
   select(-X) %>% 
   mutate(names = tolower(names))
 
 
 
-search_scotnames <- function(name) {
-  name <-tolower(name)
-  x <- enquo(name)
-  
-  temp <- scot_names %>% 
-    filter(names == !!x)
-  
-  ifelse(nrow(temp) ==0, 
-         paste0("Of 49,857 babies born in Scotland in 2019, none were registered with the name ", name),
-         paste0(temp$names, " is a ", temp$gender, " name, ranked ",
-                temp$rank, " of the 49,857 babies registered in Scotland in 2019, with ", temp$number, " babies born."))
 
-}
-
-
-
-search_names <- function(name) {
+how_popular <- function(name) {
   name <- tolower(name)
   x <- enquo(name)
   
   temp <- all_names %>% 
     filter(names == !!x) 
   
-  ifelse(nrow(temp) ==0, 
-         paste0("In 2019, there were 49,857 babies born in Scotland, and 583,969 babies born in the Rest of the UK. None were regstered with the name ", name),
-         paste0(temp$names, " is a ", temp$gender, " name, ranked ",
-                temp$rank, " of the 49,857 babies registered in ", temp$country, " in 2019, with ", temp$number, " babies born."))
+  print(paste0("In 2019, there were 49,857 babies born in Scotland, and 583,969 babies born in the Rest of the UK"))
+  
+  if(nrow(temp) == 0){print(paste0("None were regstered with the name ", name))}
+  
+  if(nrow(temp) >0){print(paste0(temp$names, " was a ", temp$gender, " name, ranked ", temp$rank, " of the baby names registered in ", 
+                           temp$country, " in 2019, with ", temp$number, " babies born."))}
   
 }
 
+how_popular("Matt")
 
 
 
-
-search_all <- function(name) {
-  name <- tolower(name)
-  x <- enquo(name)
-  
-  temp <- all_names %>% 
-    filter(names == !!x) 
-  
-  paste0(temp$names, " is a ", temp$gender, " name, ranked ",
-                temp$rank, " of the 49,857 babies registered in ", temp$country, 
-         " in 2019, with ", temp$number, " babies born.")
-  
-}
-
-search_names("Charlie")
-
-search_all("Matt")
-
-
-search_scotnames("Jamie")
